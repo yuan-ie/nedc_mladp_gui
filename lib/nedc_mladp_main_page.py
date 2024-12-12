364dfd24e39a87b72fe8d12c5122e099fea0b5cb
yuan-ie <angela.nghiem1@gmail.com>
2024-12-11 19:25:20 -0500

    updated display block. still documenting main page

--- lib/nedc_mladp_main_page.py
+++ lib/nedc_mladp_main_page.py
@@ -10,99 +10,120 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Initial slected image index.
+        #
         self.image_idx = 0
 
         # Create manager objects
+        #
         self.display_block_manager = DisplayBlockManager()
         self.stats_block_manager = StatsBlockManager()
 
         # Set window properties
-        # self.setWindowTitle("Machine Learning Applications in Digital Pathology GUI")
-        self.setWindowTitle("Yuan Test GUI")
-        self.setGeometry(20, 20, 1600, 900)
+        #
+        self.setWindowTitle("Machine Learning Applications in Digital Pathology GUI")
+        # self.setWindowTitle("Yuan Test GUI")
+        self.setGeometry(0, 0, 1600, 900)
         self.setStyleSheet("background-color: #f0d1e1;")
 
+        # Central widget. Holds ALL the page layouts.
+        #
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        # Overall window is scrollable.
+        #
         scroll_area = QScrollArea()
         scroll_area.setWidgetResizable(True)
         scroll_area.setContentsMargins(20, 20, 20, 20)
         scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
 
-        # Central widget
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
-
+        
         # Main layout
         main_layout = QVBoxLayout()
         content_widget = QWidget()
         content_layout = QVBoxLayout()
         
 
-        # block one -- title block
+        # Main Window title block -- Displays title.
+        #
         block_one = hd.title_block()
         content_layout.addWidget(block_one, 0)  # Stretch factor = 0, no extra space is given
 
-        # block two -- menu bar
-        block_two, about_button, data_button, types_button = hd.menu_block()
+        # Main window menu block -- Displays page buttons.
+        #
+        block_two, about_button, viewer_button, types_button = hd.menu_block()
         content_layout.addLayout(block_two)
 
-
-        # block three -- stacked widget
+        # Main window page block. -- Displays content based on the page picked in the menu block.
+        #
         self.stacked_widget = QStackedWidget()
 
-        # Create widgets for each view
         self.about_widget = self.create_about_widget()
-        self.data_widget = self.create_data_widget()
+        self.viewer_widget = self.create_viewer_widget()
         self.types_widget = self.create_types_widget()
         self.more_widget = self.create_more_widget()
-        self.more_widget.setWindowTitle("View More Slides")
 
-        # Add widgets to the stacked widget
         self.stacked_widget.addWidget(self.about_widget)
-        self.stacked_widget.addWidget(self.data_widget)
+        self.stacked_widget.addWidget(self.viewer_widget)
         self.stacked_widget.addWidget(self.types_widget)
-        # self.stacked_widget.addWidget(self.more_widget)
-        content_layout.addWidget(self.stacked_widget)
 
+        content_layout.addWidget(self.stacked_widget)
         content_layout.addStretch(1)
 
         # Set the central widget layout
+        #
         content_widget.setLayout(content_layout)
         scroll_area.setWidget(content_widget)
         main_layout.addWidget(scroll_area)
         central_widget.setLayout(main_layout)
 
-        # Connect buttons to corresponding slots
+        # Connect buttons to corresponding pages.
+        #
         about_button.clicked.connect(self.show_about)  # About Me button
-        data_button.clicked.connect(self.show_data)   # Viewer button
+        viewer_button.clicked.connect(self.show_viewer)   # Viewer button
         types_button.clicked.connect(self.show_types)   # Types button
         
     def create_about_widget(self):
-        about_widget = QWidget()
+        '''
+        Method:
+            Displays the content of the 'about' page.
 
+        Returns:
+            :about_widget: Widget-Type containing the 'about' content.
+        '''
+
+        about_widget = QWidget()
         about_layout = hd.about_block()
-        
         about_widget.setLayout(about_layout)
 
         return about_widget
     
-    def create_data_widget(self):
+    def create_viewer_widget(self):
+        '''
+        Method:
+            Displays the content of the 'viewer' page.
 
-        data_widget = QWidget()
-        data_layout = QVBoxLayout()
+        Returns:
+            :about_widget: Widget-Type containing the 'viewer' content.
+        '''
 
-        # Display the data
+        viewer_widget = QWidget()
+        viewer_layout = QVBoxLayout()
+
+        # Display the viewer
         display_block, self.button_select, self.button_more, self.button_save = self.display_block_manager.display_block()
-        data_layout.addLayout(display_block)
-        data_layout.addSpacing(20)
+        viewer_layout.addLayout(display_block)
+        viewer_layout.addSpacing(20)
 
         stats_block = self.stats_block_manager.stats_block()
-        data_layout.addLayout(stats_block)
+        viewer_layout.addLayout(stats_block)
 
-        data_widget.setLayout(data_layout)
+        viewer_widget.setLayout(viewer_layout)
 
         self.button_select.clicked.connect(self.display_selected_item)
         
-        return data_widget
+        return viewer_widget
     
     def create_more_widget(self):
     
@@ -110,8 +131,8 @@ class MainWindow(QMainWindow):
         more_layout = QVBoxLayout()
     
         # Display more
-        more_block, left_button, right_button = self.display_block_manager.display_more()
-        more_layout.addLayout(more_block)
+        more_block, left_button, right_button = self.display_block_manager.more_block()
+        more_layout.addWidget(more_block)
         more_widget.setLayout(more_layout)
         more_widget.hide()
 
@@ -122,6 +143,13 @@ class MainWindow(QMainWindow):
         return more_widget
     
     def create_types_widget(self):
+        '''
+        Method:
+            Displays the content of the 'data types'' page.
+
+        Returns:
+            :about_widget: Widget-Type containing the 'data types' content.
+        '''
         
         types_widget = QWidget()
         types_layout = QVBoxLayout()
@@ -136,8 +164,8 @@ class MainWindow(QMainWindow):
     def show_about(self):
         self.stacked_widget.setCurrentWidget(self.about_widget)
 
-    def show_data(self):
-        self.stacked_widget.setCurrentWidget(self.data_widget)
+    def show_viewer(self):
+        self.stacked_widget.setCurrentWidget(self.viewer_widget)
 
     def show_types(self):
         self.stacked_widget.setCurrentWidget(self.types_widget)
--- src/functs/nedc_mladp_display_block/nedc_mladp_display_block.py
+++ src/functs/nedc_mladp_display_block/nedc_mladp_display_block.py
@@ -6,7 +6,7 @@ import os
 from nedc_mladp_zoom_funct import ImageZoomManager
 
 imagepath = "/data/isip/exp/tuh_dpath/exp_0289/Machine-Learning-Applications-In-Digital-Pathology/nedc_mladp/data/yuan_test/original_files/"
-imagepath_OG = "/data/isip/exp/tuh_dpath/exp_0289/Machine-Learning-Applications-In-Digital-Pathology/nedc_mladp/data/yuan_test/original_ann_files/"
+imagepath_ANN = "/data/isip/exp/tuh_dpath/exp_0289/Machine-Learning-Applications-In-Digital-Pathology/nedc_mladp/data/yuan_test/original_ann_files/"
 imagepath_CNN = "/data/isip/exp/tuh_dpath/exp_0289/Machine-Learning-Applications-In-Digital-Pathology/nedc_mladp/data/yuan_test/cnn_files/"
 imagepath_RNF = "/data/isip/exp/tuh_dpath/exp_0289/Machine-Learning-Applications-In-Digital-Pathology/nedc_mladp/data/yuan_test/rnf_files/"
 
@@ -15,6 +15,17 @@ rnf_list_file = "/data/isip/exp/tuh_dpath/exp_0289/Machine-Learning-Applications
 xml_list_file = "/data/isip/exp/tuh_dpath/exp_0289/Machine-Learning-Applications-In-Digital-Pathology/nedc_mladp/data/yuan_test/all_files/xml_files.list"
 
 class DisplayBlockManager:
+
+    '''
+    Class: This controls the graphic display and buttons in the 'Viewer' page.
+
+    Main blocks in the DisplayBlockManager:
+        display_block:
+            - image_block: displays the selected image from the list.
+            - list_block: list of breast tissue images as JPEG.
+            - more_block: separate window that displays more graphics of selected image.
+            
+    '''
     def __init__(self):
         '''
         Constructor:
@@ -41,73 +52,77 @@ class DisplayBlockManager:
     def display_block(self):
         '''
         Method:
-            Create the display block.
+            Create the display block, which contains the image block and list block.
 
         Returns:
-            :display: display block layout.
+            :display: Layout-type.
         '''
 
         # Initializa the layout.
         display = QVBoxLayout()
-        window_layout = QHBoxLayout()
-        window_layout.setSpacing(30)  # Space between buttons
+        self.display_layout = QHBoxLayout()
+        self.display_layout.setSpacing(30)  # Space between buttons
 
         # Split this block in two layouts.
-        window_layout = self.image_block(window_layout)
-        window_layout, button_select, button_more, button_save = self.list_block(window_layout)
-        display.addLayout(window_layout)
+        image_block = self.image_block()
+        list_block, button_select, button_more, button_save = self.list_block()
+
+        self.display_layout.addWidget(image_block)
+        self.display_layout.addLayout(list_block)
+        display.addLayout(self.display_layout)
 
         return display, button_select, button_more, button_save
 
-    def image_block(self, display_window):
+    def image_block(self):
         '''
         Method:
             Create the image block -- the left side of display block.
 
-        Args:
-            :display_window: display block layout.
-
         Returns:
-            :display_window: display block layout with image block added.
+            :image_window: Layout-type.
         '''
 
-        # Initialize the image block layout.
+        # Initialize the image window within the 'Viewer' page.
+        #
         self.image_window = QGroupBox("Image Window")
         self.image_window.setStyleSheet("background-color: lightblue;")
         self.image_window.setFixedSize(900, 430)
-        self.image_window_layout = QVBoxLayout()
 
         # Default text for no image.
+        #
         self.image_label = QLabel("No Image Selected.")
         self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.image_label.setStyleSheet("font-size: 16px; font-style: italic;")
 
-        # Add the elements to the display window layout.
-        self.image_window_layout.addWidget(self.image_label)
-        self.image_window.setLayout(self.image_window_layout)
-        display_window.addWidget(self.image_window)
+        # Add the elements to the image layout.
+        #
+        self.image_layout = QVBoxLayout()
+        self.image_layout.addWidget(self.image_label)
+        self.image_window.setLayout(self.image_layout)
 
-        return display_window
+        return self.image_window
 
-    def list_block(self, display_window):
+    def list_block(self):
         '''
         Method:
             Create the list block -- the left side of display block.
             List of images to choose from.
             Buttons to select image, display more information, and save image.
 
-        Args:
-            :display_window: display block layout.
-
         Returns:
-            :display_window: display block layout with list block and buttons added.
+            :list_layout: Layout-type. Contains both scrollable list and buttons.
+            :button_select: ButtonWidget-Type.
+            :button_more: ButtonWidget-Type.
+            :button_save: ButtonWidget-Type.
         '''
 
-        # Initialize the list block layout.
+        # List block layout.
+        #
         self.list_layout = QVBoxLayout()
-        list_widget = self.dummy_list()
+        list_widget = self.image_list()
 
-        # Add the buttons.
+        # Button layout.
+        #
         buttons = QHBoxLayout()
         button_select = QPushButton("Select")
         button_more = QPushButton("More")
@@ -116,17 +131,30 @@ class DisplayBlockManager:
         buttons.addWidget(button_more)
         buttons.addWidget(button_save)
 
+        # Add the elements to the list block layout.
+        #
         self.list_layout.addWidget(list_widget)
         self.list_layout.addLayout(buttons)
 
-        display_window.addLayout(self.list_layout)
-
-        return display_window, button_select, button_more, button_save
+        return self.list_layout, button_select, button_more, button_save
     
     def track_selection(self, item):
+        '''
+        Method:
+            Track the selection of the item in the scrollable list.
+        '''
+
         self.selected_item = item
 
     def display_selected_item(self):
+        '''
+        Method:
+            Display the selected item from the list (when the 'select' button is clicked).
+            Display the original image (no annotations).
+        '''
+
+        # If an image was selected:
+        #
         if self.selected_item:
             self.image_name = self.selected_item.text()
             self.image_window.setTitle(self.image_name)
@@ -148,90 +176,101 @@ class DisplayBlockManager:
 
                 # Displays selected image by default on 'more' window popup.
                 #
-                image_path = self.more_dummy_list(0)
+                image_path = self.selected_image_list(0)
                 self.more_title.setText(self.image_title)
                 self.image_zoom_manager.update_image(image_path)
 
             else:
-                # self.image_label.setText("Image not found.")
-                # self.more_label.setText("Image not found.")
-                # self.image_label.setText("New.")
-                # self.more_label.setText("New.")
                 pass
 
         elif self.selected_item is None:
             self.image_label.setText("No item selected. Please select an item first.")
 
-    def display_more(self):
+    def more_block(self):
         '''
         Method:
             This method activates for each 'more' button click.
-            Creates a new window with the layout of title, image, and buttons.
+            Creates a new window with the layout of title, image, buttons, and legend.
             Starts off with the original image that is selected.
+
+        Returns:
+            :more_block: Widget-Type that contains the whole 'more' window layout.
+            :left_button: ButtonWidget-Type.
+            :right_button: ButtonWidget-Type.
         '''
 
-        more_layout = QVBoxLayout()
-        
-        more_window_layout =  QVBoxLayout()
+        # Overall 'more' window containing the overall layout.
+        #
+        more_window = QGroupBox()
+        more_window.setStyleSheet("background-color: lightblue;")
+        more_window.setGeometry(20, 20, 900, 800)
+        more_window.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.more_window = QGroupBox()
-        self.more_window.setStyleSheet("background-color: lightblue;")
-        # self.more_window.setFixedSize(900,800)
-        self.more_window.setGeometry(20, 20, 900, 800)
-        self.more_window.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        # Overall layout containing the 'more' functions.
+        #
+        more_layout =  QVBoxLayout()
 
+        # Title of the toggled image in the 'more' window'.
+        #
         self.more_title = QLabel()
         self.more_title.setText("Original Biopsy Slide")
         self.more_title.setFixedHeight(20)
         self.more_title.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.more_title.setStyleSheet("font-size: 16px;") # font-weight: bold;
 
-        # Display image
-        # self.more_label = QLabel()
-        # self.more_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        # Legend of the 'more' window.
+        #
+        self.more_legend = self.legend_chart()
+        self.more_legend.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.legend = self.legend_chart()
-        self.legend.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.focus_graphic = QVBoxLayout()
-        self.focus_graphic.addWidget(self.image_zoom_manager)
-        self.focus_graphic.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        # Toggled image of the 'more' window'. Has the zoom and drag functionality.
+        #
+        self.more_image = QVBoxLayout()
+        self.more_image.addWidget(self.image_zoom_manager)
+        self.more_image.setAlignment(Qt.AlignmentFlag.AlignCenter)
         
-        # Add the previous and next buttons
+        # Previous and Next buttons for the 'more' window.
+        #
         more_buttons = QHBoxLayout()
         left_button = QPushButton("Previous")
         left_button.setFixedSize(100,100)
-        # left_button.setFixedWidth(100)
         right_button = QPushButton("Next")
         right_button.setFixedSize(100,100)
-        # right_button.setFixedWidth(100)
         more_buttons.addWidget(left_button)
         more_buttons.addWidget(right_button)
-        more_buttons.addWidget(self.legend)
+        more_buttons.addWidget(self.more_legend)
 
-        more_window_layout.addWidget(self.more_title,0)
-        more_window_layout.setSpacing(10)
-        more_window_layout.addLayout(self.focus_graphic)
-        more_window_layout.setSpacing(30)
-        # more_window_layout.addWidget(self.legend, alignment=Qt.AlignmentFlag.AlignCenter)
-        more_window_layout.addLayout(more_buttons)
+        # Add all components to the layout and add layout to the 'more' block.
+        #
+        more_layout.addWidget(self.more_title,0)
+        more_layout.setSpacing(10)
+        more_layout.addLayout(self.more_image)
+        more_layout.setSpacing(30)
+        more_layout.addLayout(more_buttons)
+        more_layout.addStretch(1)
+        more_window.setLayout(more_layout)
 
-        more_window_layout.addStretch(1)
-        self.more_window.setLayout(more_window_layout)
-
-        more_layout.addWidget(self.more_window)
-
-        return more_layout, left_button, right_button
+        return more_window, left_button, right_button
     
     def display_more_items(self, idx):
         '''
         Method:
-            Activates when 'prev' or 'next' is clicked.
+            Activates when 'prev' or 'next' button is clicked.
+            Displays the new image within the 'more' window.
+
+        Arguments:
+            :idx: Index of the graphic for each image.
+                (0) Original Image
+                (1) Original Image w/ Annotations
+                (2) RNF Predictions
+                (3) CNN Predictions
         '''
 
+        # Display image if available.
+        #
         if self.selected_item:
 
-            image_path = self.more_dummy_list(idx)
+            image_path = self.selected_image_list(idx)
             self.pixmap = QPixmap(image_path)
             if not self.pixmap.isNull():
 
@@ -239,43 +278,77 @@ class DisplayBlockManager:
                 #
                 self.more_title.setText(self.image_title)
                 self.image_zoom_manager.update_image(image_path)
-
             else:
                 pass
-                # self.more_title.setText("No Image")
-                # self.more_label.setText("Image not found.")
         else:
             pass
-            # self.more_label.setText("No item selected. Please select an item first.")
 
-    def dummy_list(self):
+    def image_list(self):
+        '''
+        Method:
+            Create list of all the image names in the directory (without the whole path).
+        
+        Returns:
+            :list_widget: Scrollable list Widget-Type.
+        '''
 
         # List widget for the scrollable list.
+        #
         list_widget = QListWidget()
 
+        # Track the selection when an item is clicked.
+        #
         list_widget.itemClicked.connect(self.track_selection)
+
+        # Add all images to list.
+        #
         for image in os.listdir(imagepath):
             list_widget.addItem(image)
 
-        # Add fixed number of items
-        # for i in range(20):  # Adjust the number as needed
-        #     list_widget.addItem(f"Image {i + 1}")
-
         return list_widget
     
-    def more_dummy_list(self, idx):
+    def selected_image_list(self, idx):
+        '''
+        Method:
+            Get the next or previous graphic of the selected image.
+
+        Arguments:
+            :idx: Index of the graphic for each image.
+                (0) Original Image
+                (1) Original Image w/ Annotations
+                (2) RNF Predictions
+                (3) CNN Predictions
+
+        Returns:
+            :selected_image[idx]: Indexed image.
+        '''
+
+        # Concatanate the file path to the file name.
+        #
         image = imagepath + self.image_name
-        image_OG = imagepath_OG + self.image_name
+        image_ANN = imagepath_ANN + self.image_name
         image_RNF = imagepath_RNF + self.image_name
         image_CNN = imagepath_CNN + self.image_name
-        more_image_list = [image, image_OG, image_RNF, image_CNN]
-        more_image_title = ["Original Biopsy Slide", "Original Annotations", "RNF Predictions", "CNN Predictions"]
-        self.image_title = more_image_title[idx]
-        print(idx, ": ", more_image_list[idx], "| original image: ", self.image_name)
 
-        return more_image_list[idx]
+        # Select through the correct file to display.
+        #
+        selected_image = [image, image_ANN, image_RNF, image_CNN]
+        more_image_title = ["Biopsy Slide", "Pathologist Annotations", "RNF Predictions", "CNN Predictions"]
+        self.image_title = more_image_title[idx]
+
+        return selected_image[idx]
     
     def get_filenames(self):
+        '''
+        Method:
+            Get the whole XML, CSV (CNN), and CSV (RNF) filepaths of the selected image.
+            These paths are to be used by the stats block to display accuracies.
+
+        Returns:
+            :original_xml_file: XML file.
+            :cnn_pred_file: CSV file.
+            :rnf_pred_file: CSV file.
+        '''
         file, extension = os.path.splitext(self.image_name)
 
         for line in self.xml_list:
@@ -301,7 +374,6 @@ class DisplayBlockManager:
         grid_box = QGroupBox()
         grid_box.setStyleSheet("background-color: white;")
         grid_box.setFixedWidth(400)
-        # grid_box.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         bckg_ref, bckg_hyp = "#ff9603", "#ffcb82"
         norm_ref, norm_hyp = "#ffdd00", "#f7eb9e"
